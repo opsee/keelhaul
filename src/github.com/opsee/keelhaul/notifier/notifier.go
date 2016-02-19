e56e1a8e5e78bcda36a46b60895d71aa52c01ca6
Michael Borsuk <mrb@boundary.com>
2016-02-19 12:59:13 -0800

    Revert "use path.Join"
    This reverts commit 9163594a8eefeb176e00b618442ff72bbfafe603.

--- src/github.com/opsee/keelhaul/notifier/notifier.go
+++ src/github.com/opsee/keelhaul/notifier/notifier.go
@@ -9,7 +9,6 @@ import (
 	slacktmpl "github.com/opsee/notification-templates/dist/go/slack"
 	log "github.com/sirupsen/logrus"
 	"net/http"
-	"path"
 )
 
 type Notifier interface {
@@ -73,7 +72,7 @@ func init() {
 }
 
 func (n *notifier) NotifySlackBastionState(isUp bool, custID string, notifyVars map[string]interface{}) error {
-	resp, err := http.Get(path.Join(n.VapeUserInfoEndpoint, custID))
+	resp, err := http.Get(n.VapeUserInfoEndpoint + "/" + custID)
 	if err != nil {
 		return err
 	}
