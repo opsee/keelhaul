e9fc4d86ed51e00840691e52ac54e0b9fa95dd2c
Mark Martin <assplecake@gmail.com>
2016-02-27 16:47:12 -0800

    actually do the listing

--- src/github.com/opsee/keelhaul/service/admin.go
+++ src/github.com/opsee/keelhaul/service/admin.go
@@ -1,10 +1,36 @@
 package service
 
 import (
+	"github.com/opsee/basic/schema"
 	opsee "github.com/opsee/basic/service"
+	opsee_types "github.com/opsee/protobuf/opseeproto/types"
+	log "github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 )
 
 func (s *service) ListBastionStates(ctx context.Context, req *opsee.ListBastionStatesRequest) (*opsee.ListBastionStatesResponse, error) {
-	return nil, nil
+	bs, err := s.db.ListBastionStates(req.CustomerIds)
+	if err != nil {
+		log.WithError(err).Error("failed to list bastion states")
+		return nil, err
+	}
+
+	bastionStates := make([]*schema.BastionState, len(bs.States))
+	for i, s := range bs.States {
+		ts := &opsee_types.Timestamp{}
+		err = ts.Scan(s.LastSeen)
+		if err != nil {
+			log.WithError(err).Error("failed scanning bastion_state last_seen timestamp")
+			return nil, err
+		}
+
+		bastionStates[i] = &schema.BastionState{
+			Id:         s.ID,
+			CustomerId: s.CustomerID,
+			Status:     s.Status,
+			LastSeen:   ts,
+		}
+	}
+
+	return &opsee.ListBastionStatesResponse{BastionStates: bastionStates}, nil
 }
