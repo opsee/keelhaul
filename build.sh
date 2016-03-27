6bb6ca6951b23739e51a0800c0e6fbb8baafc4e0
Mark Martin <assplecake@gmail.com>
2016-03-27 13:31:40 -0700

    govendor plus go16

--- build.sh
+++ build.sh
@@ -5,11 +5,3 @@ echo "loading schema for tests..."
 echo "create database keelhaul_test" | psql $KEELHAUL_POSTGRES_CONN
 #echo "drop database if exists keelhaul_test; create database keelhaul_test" | psql $KEELHAUL_POSTGRES_CONN
 migrate -url $KEELHAUL_POSTGRES_CONN -path ./migrations up
-
-git submodule update --init
-
-proto_dir=proto/bastion_proto
-checker_proto=${proto_dir}/checker.proto
-protoc --go_out=plugins=grpc,Mgoogle/protobuf/descriptor.proto=github.com/golang/protobuf/protoc-gen-go/descriptor:. ${checker_proto}
-
-mv ./proto/bastion_proto/checker.pb.go src/github.com/opsee/keelhaul/checker/checker.pb.go
