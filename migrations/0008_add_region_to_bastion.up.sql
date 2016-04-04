93ae9e5e3156a1c0963f31c4bd53cbf490c0c4fd
Mark Martin <assplecake@gmail.com>
2016-04-04 13:57:22 -0700

    migrate data manually

--- migrations/0008_add_region_to_bastion.up.sql
+++ migrations/0008_add_region_to_bastion.up.sql
@@ -1,3 +1,1 @@
-alter table bastions add column region character varying(24) default '';
-update bastions set region = (select region from regions where bastions.customer_id = regions.customer_id order by created_at desc limit 1);
-alter table bastions alter column region set not null;
+alter table bastions add column region character varying(24) not null default '';
