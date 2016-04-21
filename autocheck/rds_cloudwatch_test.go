3e3e2725e903bd534080a04dcb97da3ee8a2bcb4
Mike Borsuk <mike@opsee.co>
2016-04-20 18:39:27 -0700

    a dumb little test

--- autocheck/rds_cloudwatch_test.go
+++ autocheck/rds_cloudwatch_test.go
@@ -3,17 +3,15 @@ package autocheck
 import (
 	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/service/rds"
-	"github.com/opsee/basic/schema"
-	opsee_types "github.com/opsee/protobuf/opseeproto/types"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
 
 var rdsTests = []struct {
-	rds *rds.RDS
+	rds *rds.DBInstance
 }{
 	{
-		rds: &rds.RDS{
+		rds: &rds.DBInstance{
 			DBInstanceIdentifier: aws.String("opsee-test-db"),
 			DBInstanceClass:      aws.String("db.m3.xlarge"),
 		},
@@ -24,7 +22,7 @@ func TestRDSGenerate(t *testing.T) {
 	assert := assert.New(t)
 
 	for _, t := range rdsTests {
-		checks, err := NewTarget(t.rds).Generate()
+		_, err := NewTarget(t.rds).Generate()
 		assert.NoError(err)
 	}
 }
