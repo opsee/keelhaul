598d4ea0ddb4c035fbc14da67ea63c762b19bf93
Mark Martin <assplecake@gmail.com>
2016-05-06 15:51:06 -0700

    use the spanx provider (#83)
    * use the spanx provider
    
    * update basic

--- service/stacks.go
+++ service/stacks.go
@@ -2,9 +2,9 @@ package service
 
 import (
 	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
 	"github.com/aws/aws-sdk-go/aws/session"
 	opsee "github.com/opsee/basic/service"
+	"github.com/opsee/spanx/spanxcreds"
 	"golang.org/x/net/context"
 )
 
@@ -38,23 +38,10 @@ func (s *service) LaunchStack(ctx context.Context, req *opsee.LaunchStackRequest
 		req.InstanceSize = "t2.micro"
 	}
 
-	// get creds from spanx
-	stscreds, err := s.spanx.GetCredentials(ctx, &opsee.GetCredentialsRequest{
-		User: req.User,
-	})
-
-	if err != nil {
-		return nil, err
-	}
-
 	sess := session.New(&aws.Config{
-		Credentials: credentials.NewStaticCredentials(
-			stscreds.Credentials.GetAccessKeyID(),
-			stscreds.Credentials.GetSecretAccessKey(),
-			stscreds.Credentials.GetSessionToken(),
-		),
-		Region:     aws.String(req.Region),
-		MaxRetries: aws.Int(11),
+		Credentials: spanxcreds.NewSpanxCredentials(req.User, s.spanx),
+		Region:      aws.String(req.Region),
+		MaxRetries:  aws.Int(11),
 	})
 
 	_, err = s.launcher.LaunchBastion(sess, req.User, req.Region, req.VpcId, req.SubnetId, req.SubnetRouting, req.InstanceSize, "stable")
